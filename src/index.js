a3a725eb73abb3896f468423e48c7326ae77f132
Michaela <66178768+WunschCoding@users.noreply.github.com>
2025-10-19 19:38:22 +0200

    removed forecast from html and created in javascript by html injection

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function searchCity(event) {
     axios.get(apiUrl).then(getWeatherData);
 
     let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
-    //axios.get(apiUrlForecast).then(getWeatherForecast);
+    axios.get(apiUrlForecast).then(getWeatherForecast);
   } else {
     alert("Please enter at least 3 characters");
   }
@@ -52,7 +52,7 @@ function getWeatherForecast(response) {
   let forecastDays = document.querySelectorAll(".weather-forecast-day");
 
   for (let i = 0; i < forecastDays.length; i++) {
-    let forcastDayName = forecastDays[i].querySelector("#forecast-day-name");
+    let forcastDayName = forecastDays[i].querySelector(".forecast-day-name");
     if (actualDay + i <= 6) {
       forcastDayName.innerHTML = dayNumberToDayname(actualDay + i).substring(
         0,
@@ -61,28 +61,22 @@ function getWeatherForecast(response) {
     } else {
       forcastDayName.innerHTML = dayNumberToDayname(0).substring(0, 3);
     }
-    let forecastIconDay = forecastDays[i].querySelector("#forecast-icon-day");
+    let forecastIconDay = forecastDays[i].querySelector(".forecast-icon-day");
     forecastIconDay.src = response.data.daily[i].condition.icon_url;
     let forecastMaxTemperatureDay = forecastDays[i].querySelector(
-      "#forecast-max-temperature-day"
+      ".forecast-max-temperature-day"
     );
     forecastMaxTemperatureDay.innerHTML = Math.round(
       response.data.daily[i].temperature.maximum
     );
     let forecastMinTemperatureDay = forecastDays[i].querySelector(
-      "#forecast-min-temperature-day"
+      ".forecast-min-temperature-day"
     );
     forecastMinTemperatureDay.innerHTML = Math.round(
       response.data.daily[i].temperature.minimum
     );
     //console.log(response.data.daily[i].temperature.minimum);
   }
-}
-
-function insertForecastData(element) {
-  let actualDay = new Date().getDay();
-  let elementName = element.querySelector("#forecast-day-name");
-  elementName.innerHTML = dayNumberToDayname(actualDay + 1).substring(0, 3);
 }
 
 function round(value, precision) {
@@ -115,6 +109,41 @@ function dayNumberToDayname(dayNumber) {
   ];
   return dayNames[dayNumber];
 }
+
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `          
+          <div class="weather-forecast-day">
+            <div class="forecast-day-name">${day}</div>
+            <div>
+              <img
+                class="forecast-icon-day"
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt="weather-icon"
+              />
+            </div>
+            <div class="forecast-temperature-day">
+              <div class="forecast-max-temperature-day">20°</div>
+              <div
+                class="forecast-min-temperature-day"
+              >
+                10°
+              </div>
+            </div>
+          </div>
+`;
+  });
+  forecast.innerHTML = forecastHtml;
+}
+
+displayForecast();
+
 firstCity();
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
